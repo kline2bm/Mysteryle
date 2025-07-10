484689b49674878699fba6649207c52605348c62
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2025-07-09 22:20:35 -0400

    Create dateTime.js

---
+++ dateTime.js
@@ -0,0 +1,6 @@
+const _dateTime = document.getElementById("_dateTime"), _birthday = document.getElementById("_birthday");
+setInterval(_ => _dateTime.innerText = new Date(), 1_000);
+const BIRTHDAYS = [
+  // [mm, dd, name]
+];
+// TODO
