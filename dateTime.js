b92b1eb5bd80716a9d7987393b8ed6b5e8dfb126
Bryce Kline <161635434+kline2bm@users.noreply.github.com>
2025-08-08 21:09:52 -0400

    Update dateTime.js

--- dateTime.js
+++ dateTime.js
@@ -1,6 +1,81 @@
 const _dateTime = document.getElementById("_dateTime"), _birthday = document.getElementById("_birthday");
-setInterval(_ => _dateTime.innerText = new Date(), 1_000);
+setInterval(_ => _dateTime.innerText = new Date().toString().split(" GMT")[0], 1_000);
 const BIRTHDAYS = [
-  // [mm, dd, name]
+  // January
+  [
+    [12, "rockratzero"],
+    [16, "Panthera"],
+    [23, "Mathpesto"]
+  ],
+  // February
+  [
+    [10, "Andrewsarchus"],
+    [14, "Palfly Kampling"],
+    [17, "it_is_donatello_86"],
+    [20, "Olima"],
+    [25, "Yoshi Baroshi"]
+  ],
+  // March
+  [
+    [29, "Missing a few cards"]
+  ],
+  // April
+  [
+    [3, "AnalyticalNinja"],
+    [13, "Scott Strosahl"],
+    [25, "Malrog"],
+    [29, "Mitchell"]
+  ],
+  // May
+  [
+    [3, "Bryce Kline"],
+    [28, "SamuPiano"]
+  ],
+  // June
+  [
+    [20, "Sven (Killroy)"]
+  ],
+  // July
+  [3, "Peachy and Rhonda"],
+  [4, "Solver Tom"],
+  [7, "I Love Sleeping - Leeor"],
+  [10, "Matt y Ddraig"],
+  [30, "Wisteria"]
+  // August
+  [
+    [4, "Ambrose"],
+    [10, "Yura (Chameleon)"],
+    [25, "Piatato"],
+    [26, "Dumediat"],
+    [30, "Raumplaner"]
+  ],
+  // September
+  [
+    [7, "Myxo"],
+    [17, "AkashG23"],
+    [18, "Lilly Clamage"],
+    [23, "Bellsita"],
+    [29, "heliopolix"],
+    [30, "MaverickJd and Rangsk (David Clamage)"]
+  ],
+  // October
+  [
+    [11, "RockyRoer"],
+    [14, "Timotab"],
+    [16, "Chilly"]
+    [26, "mnasti2"]
+  ],
+  // November
+  [
+    [23, "Fulldeck"]
+  ],
+  // December
+  [
+    [1, "Bremster"],
+    [2, "Many_Splendored"],
+    [3, "Aspartagcus"],
+    [9, "TheAsylm"],
+    [14, "Virtual"],
+    [30, "Sfushidahardy"]
 ];
 // TODO
